b67e109e9d10f670fc8f870aac1a60d8bdf856bf
desfeк <desfe@DESKTOP-86DPKL3>
2023-01-27 21:22:24 +0500

    lv

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ void func(Parent& other) {
 }
 int main() {
 	Parent parent;
-	Child child;
+	Child child;//нелья сразу даункастить Parent в Child так как Parent не знает о существовании Child и его функции и в С -Style это UB
 	//upcast (не¤вное преобразование типов разрешено)
 	//варианты создани¤ в стэке и куче(в стеке может удалитьс¤)после выхода за размер кода автоматически вызоветс¤ и нструктор
 	Parent* pParent = &child;//стэк
@@ -31,9 +31,9 @@ int main() {
 	//C- style каст выполн¤ет первый успешный каст в пор¤лк:
 	//cost_cast - снимает константность
 	//static_cast - когда выполн¤ет не обращает внимание на модификаторы public, private  и т.д. если пытатьс¤ скоставать не св¤занные между собой классы это UB
-	//static_cast - > const_cast
+	//static_cast -> const_cast
 	//reinterpret_cast
-	//reinterpret_cast- > const_cast
+	//reinterpret_cast -> const_cast
 	Child* pChild2 = static_cast<Child*>(pParent);//если классы в иерархии наследовани¤ не виртуальные то можно  и нужно кастовать(upcast/downcast) через static_cast
 	//downcast UB
 	//Child* pChild = (Child*)&pParent;
@@ -54,13 +54,26 @@ int main() {
 	//или (double)a
 	std::cout << "b: " << b << std::endl;
 
-	int c = 6; 
+	int c = 6; //если данные изначально const то снятие константы с указателя привело бы к UB, c this константность можно снять если она не была константной
 	const int* pC = &c;
 	int* ppC = const_cast<int*> (pC);
 	std::cout << "=============\n";
 	std::cout << *ppC << std::endl;
 	*ppC = 4;
 	std::cout << *ppC << " " << c << std::endl;
-
+	//dynamic cast<new type> - для работы с виртуальными классами, при отсутствии наследования UB
+	//Здесь нет UB так как dynamic cast ведет себя определенным образом, проверяет что UB не возможно и сообщает об этом< через private и protected не ходит только через public
+	Child* pChild3 = dynamic_cast<Child*>(&parent);//UB в dynamic_cast когды мы пытаемся кастить 1 класс во 2 если эти касты не наследуют другу и быть виртуальными
+	if (pChild3 == nullptr) {
+		std::cout << "pChild is nullptr" << std::endl;
+	}
+	try{
+		Child& pChild5 = dynamic_cast<Child&>(parent);
+	}
+	catch (std::bad_cast& e) {
+		std::cout << "cast parent to child result in bad_cast" << std::endl;
+		std::cout << e.what() << std::endl;
+	}
+	//reinterpret_cast = пытается скастить чего бы это не стоило, лучше избегать, 100 % можно скастить 1 тип в другой и потом обратно, используют в чтении в байтовом режиме (наприме в чар)
 	return 0;
 }
